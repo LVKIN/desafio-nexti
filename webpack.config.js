41436fbd2d703751b23b3ada2f4321ad2d74f767
LVKIN <backuplucas6150@gmail.com>
2022-03-10 20:40:33 -0300

    feat: setup project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist") 
+    },
+    compress: true,
+    port: 3000
+  },
+};
